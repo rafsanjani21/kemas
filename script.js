8cdd3c79299f1c4641359688777f42394d53bd16
rafsanjani21 <rafsanjani1719@gmail.com>
2025-11-16 13:59:10 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const contactBtn = document.getElementById("contactBtn");
+
+if (contactBtn) {
+  contactBtn.addEventListener("click", () => {
+    alert("Terima kasih! Anda dapat menghubungi saya melalui email.");
+  });
+}
